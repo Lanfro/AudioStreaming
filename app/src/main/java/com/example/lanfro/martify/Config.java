4d2317ce45f55e2fa1dd103bc554aaeffad390d6
Lanfro <lanfro@msn.com>
2016-03-20 17:32:16 +0100

    Client instead of Cliend

--- app/src/main/java/com/example/lanfro/martify/Config.java
+++ app/src/main/java/com/example/lanfro/martify/Config.java
@@ -5,6 +5,6 @@ package com.example.lanfro.martify;
  */
 public class Config {
 
-    public static final String CLIENT_ID = "<ENTER YOUR OWN CLIEND_ID>";
+    public static final String CLIENT_ID = "<ENTER YOUR OWN CLIENT_ID>";
     public static final String API_URL = "https://api.soundcloud.com";
 }
